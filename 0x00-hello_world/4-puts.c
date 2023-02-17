e74c48b7cab586ae9977833d3102fd8a7ce2da0f
Ebenezer <ebenyz95@gmail.com>
2023-02-16 17:41:41 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - print a line using puts
+ * Return: 0
+ */
+init main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+
+
+}
