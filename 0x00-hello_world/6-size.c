23ebc7439fa3823ec64383b4cb99ca719b667496
Ebenezer <ebenyz95@gmail.com>
2023-02-17 04:44:44 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
- *
- * main - prints the size of various types on the computer it is compiled and run on
- *
+ * main - prints the size of various types on the computer
  * Return: 0
  */
 int main(void)
-
 {
 	printf("size of a char: %lu byte(s)\n", (sizeof(char));
 			printf("size of an int: %lu byte(s)\n", (sizeof(int));
@@ -14,4 +11,4 @@ int main(void)
 					printf("size of a long long int: %lu byte(s)\n", (sizeof(long long int));
 						printf("size of a float: %lu byte(s)\n", (sizeof(float));
 							return (0);
-							}
+}
