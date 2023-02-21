05e87011b977a0e2105c61a13f18d2974d68a9e2
Ebenezer <ebenyz95@gmail.com>
2023-02-21 10:30:59 -0800

    function nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - To write a program that prints _putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		putchar('\n');
+			return (0);
+}
