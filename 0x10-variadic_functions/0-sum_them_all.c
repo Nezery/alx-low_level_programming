6106b1486951498303e9c98891ffca8d5ed059fe
Ebenezer <ebenyz95@gmail.com>
2023-03-24 18:49:06 -0700

    vad function.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
 /**
  * sum_them_all - adds all the numbers
  * @n: the number of parameters passed
- *
  * Return: Always 0
  */
 
@@ -18,8 +17,9 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
 	va_end(ap);
 
 	return (sum);
