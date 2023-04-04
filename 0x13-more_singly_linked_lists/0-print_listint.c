11198118b71979adf2d574941e13bd28d85536f4
Ebenezer <ebenyz95@gmail.com>
2023-04-04 16:37:19 -0700

    update print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t numb = 0;
+	size_t n = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		numb++;
+		n++;
 	}
-	return (numb);
+	return (n);
 }
 
