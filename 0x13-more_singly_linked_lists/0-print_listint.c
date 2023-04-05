ff9f8bfd3cab1440a0c47de8667c51332ac4b5c5
Ebenezer <ebenyz95@gmail.com>
2023-04-04 19:13:52 -0700

    oprint list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,9 +14,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t alpha = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("->%d\n", n);
 		alpha++;
 		h = h->next;
 	}
