8710301a9e3415e99ca73be37bbac2f4f777ed29
Ebenezer <ebenyz95@gmail.com>
2023-04-05 17:33:38 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			beta = beta->next;
 		}
 		if (beta == NULL)
-			return (NULL)
+			return (NULL);
 	}
 	new =  malloc(sizeof(listint_t));
 
